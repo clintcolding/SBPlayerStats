e427db0544a81d845e7e6339f61e53684520efbd
Clint Colding <ccolding@a1hr.com>
2017-12-26 10:25:38 -0500

    Added pitchers var

--- app.py
+++ app.py
@@ -12,9 +12,10 @@ def my_form_post():
     tid = request.form['text']
 
     myteamname = (sbteamdata.get_team_data(tid)).name
-    stats = sbteamdata.get_team_stats(tid)
+    stats = sbteamdata.get_team_data(tid)
+    pitchers = sbteamdata.get_pitchers(tid)
 
-    return render_template('teamstats.html', myteamname=myteamname, stats=stats)
+    return render_template('teamstats.html', myteamname=myteamname, stats=stats, pitchers=pitchers)
 
 if __name__ == '__main__':
     app.run(debug=True)
