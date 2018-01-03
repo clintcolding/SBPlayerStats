c375df18403319bafd8c6f0f993bd0cb0e43d41a
Clint Colding <clintcolding@gmail.com>
2018-01-03 11:51:54 -0500

    Added a search route

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sbteamdata
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def my_form():
+@app.route('/')
+def index():
     return render_template('index.html')
 
-@app.route('/teamstats', methods=['POST'])
-def my_form_post():
+@app.route('/search', methods=['POST'])
+def search():
     tid = request.form['teamid']
 
+    return redirect('/stats/{0}'.format(tid))
+
+@app.route('/stats/<tid>', methods=['GET', 'POST'])
+def stats(tid):
     myteamname = (sbteamdata.get_team_data(tid)).name
     stats = sbteamdata.get_team_data(tid)
     pitchers = sbteamdata.get_pitchers(tid)
 
-    return render_template('teamstats.html', myteamname=myteamname, stats=stats, pitchers=pitchers)
+    return render_template('stats.html', myteamname=myteamname, stats=stats, pitchers=pitchers)
 
 if __name__ == '__main__':
     app.run(debug=True)
