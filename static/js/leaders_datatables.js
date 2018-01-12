631f5221fc2eae094e8279c37ce2395659ea338f
Clint Colding <clintcolding@gmail.com>
2018-01-12 09:51:41 -0500

    Added team leaders

--- static/js/leaders_datatables.js
+++ static/js/leaders_datatables.js
@@ -19,11 +19,21 @@ $(document).ready(function() {
         "scrollX": true,
         "aaSorting": []
     } );
+// Datatables for team leaders
+    $('#team-leaders').DataTable( {
+        "order": [[ 4, "desc"]],
+        "pageLength": 25,
+        "paging": true,
+        "info": true,
+        "searching": true,
+        "scrollX": true,
+        "aaSorting": []
+} );
 } );
 
 // Ranking index that persists with search
 $(document).ready(function() {
-    $('#hitting-leaders, #pitching-leaders').each(function(){ 
+    $('#hitting-leaders, #pitching-leaders, #team-leaders').each(function(){ 
         var t = $(this).DataTable();
         t.on( 'order.dt search.dt', function () {
             t.column(0, { order:'applied'}).nodes().each( function (cell, i) {
