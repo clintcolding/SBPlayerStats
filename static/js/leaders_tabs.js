da383b24e4e8019c5a7898cacab431f25b78504e
Clint Colding <clintcolding@gmail.com>
2018-01-12 09:51:50 -0500

    Added team leaders

--- static/js/leaders_tabs.js
+++ static/js/leaders_tabs.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
     $('ul.tabs li').click(function(){
         var tab_id = $(this).attr('data-tab');
-        var table = $('#pitching-leaders').DataTable();
+        var pitching_table = $('#pitching-leaders').DataTable();
+        var team_table = $('#team-leaders').DataTable();
 
         $('ul.tabs li').removeClass('current');
         $('.tab-content').removeClass('current');
         $(this).addClass('current');
         $("#"+tab_id).addClass('current');
-        table.columns.adjust().draw();
+        pitching_table.columns.adjust().draw();
+        team_table.columns.adjust().draw();
     });
 });
