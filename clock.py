678b50f0d8458d5ee37efc52bde1ecbb31ab801b
Clint Colding <clintcolding@gmail.com>
2018-01-12 16:43:05 -0500

    Updated schedule

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from update import update_stats
 
 sched = BlockingScheduler()
 
-@sched.scheduled_job('cron', hour=16, minute=30)
+@sched.scheduled_job('cron', hour=16, minute=45)
 def scheduled_job():
     print('This job updates stats daily at 11 PM.')
     update_stats()
