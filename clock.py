0c31a85610c11180e036ef4e470b7460b21d82d2
Clint Colding <clintcolding@gmail.com>
2018-01-12 14:52:45 -0500

    Initial commit

---
+++ clock.py
@@ -0,0 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from update import update_stats
+
+sched = BlockingScheduler()
+
+@sched.scheduled_job('cron', hour=23)
+def scheduled_job():
+    print('This job updates stats daily at 11 PM.')
+    update_stats()
+
+sched.start()
