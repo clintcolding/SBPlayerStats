a31ffcffc2cbe8cb74ceffb961ea842b79960c45
Clint Colding <clintcolding@gmail.com>
2018-01-25 17:38:47 -0500

    Updated script for new table columns

--- update.py
+++ update.py
@@ -14,41 +14,40 @@ def update_stats():
 
     for tid in teamids:
         stats = sbteamdata.get_team_data(tid)
-        pitchers = sbteamdata.get_pitchers(tid)
 
-        team = Team.query.filter_by(teamid=tid).first()
-        players = Player.query.filter_by(teamid=tid).all()
-        p_players = Pitcher.query.filter_by(teamid=tid).all()
+    team = Team.query.filter_by(teamid=tid).order_by(Team.season.desc()).all()
+    players = Player.query.filter_by(teamid=tid, season=stats.season).all()
+    pitchers = Pitcher.query.filter_by(teamid=tid, season=stats.season).all()
 
-        if team:
-            db.session.delete(team)
+    for entry in team:
+        if entry.season == int(stats.season):
+            db.session.delete(entry)
             db.session.commit()
 
-        if team.games != 0:
-            new_entry = Team(tid, stats.name, stats.games, stats.ab, stats.hits, stats.avg, stats.singles,
-                stats.doubles, stats.triples, stats.hr, stats.rbi, stats.so, stats.era, stats.slg, stats.wins,
-                stats.losses, stats.pct, stats.rs, stats.ra)
-            db.session.add(new_entry)
+    new_entry = Team(stats.season, tid, stats.name, stats.stars, stats.stadium, stats.pl,
+                     stats.pl_position, stats.trained, stats.games, stats.ab, stats.hits,
+                     stats.avg, stats.singles, stats.doubles, stats.triples, stats.hr, 
+                     stats.rbi, stats.so, stats.era, stats.slg, stats.wins, stats.losses,
+                     stats.pct, stats.rs, stats.ra)
+    db.session.add(new_entry)
 
-        if players:
-            for player in players:
-                db.session.delete(player)
-                db.session.commit()
+    if players:
+        for player in players:
+            db.session.delete(player)
+            db.session.commit()
 
-        for player in stats.players:
-            if player.games != "0": 
-                new_entry = Player(tid, player.id, player.name, player.position, player.games, player.ab, 
-                    player.hits, player.avg, player.singles, player.doubles, player.triples, player.hr, player.rbi, 
-                    player.so, player.era, player.slg)
-                db.session.add(new_entry)
+    for player in stats.players:
+        new_entry = Player(stats.season, tid, player.id, player.name, player.position, player.games, player.ab,
+                           player.hits, player.avg, player.singles, player.doubles, player.triples,
+                           player.hr, player.rbi, player.so, player.era, player.slg)
+        db.session.add(new_entry)
 
-        if p_players:
-            for player in p_players:
-                db.session.delete(player)
-                db.session.commit()
+    if pitchers:
         for player in pitchers:
-            if player.era != "-":
-                new_entry = Pitcher(tid, player.id, player.name, player.games, player.so, player.era)
-                db.session.add(new_entry)
-        db.session.commit()
+            db.session.delete(player)
+            db.session.commit()
+    for player in stats.pitchers:
+        new_entry = Pitcher(stats.season, tid, player.id, player.name, player.games, player.so, player.era)
+        db.session.add(new_entry)
+    db.session.commit()
     
