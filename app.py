114d2329278d6579c86400263cfbbedb32cf83b0
Clint Colding <clintcolding@gmail.com>
2018-01-30 10:24:56 -0500

    Moved db to separate file and added player career stats page

--- app.py
+++ app.py
@@ -1,131 +1,16 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, current_app
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from db import db, Team, Player, Pitcher
+import getcareerstats
 import os
 import sbteamdata
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-db = SQLAlchemy(app)
 
-class Team(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.DateTime)
-    season = db.Column(db.Integer)
-    teamid = db.Column(db.Integer)
-    name = db.Column(db.String)
-    stars = db.Column(db.Integer)
-    stadium = db.Column(db.String)
-    pl = db.Column(db.String)
-    pl_position = db.Column(db.String)
-    trained = db.Column(db.String)
-    games = db.Column(db.Integer)
-    ab = db.Column(db.Integer)
-    hits = db.Column(db.Integer)
-    avg = db.Column(db.String)
-    singles = db.Column(db.Integer)
-    doubles = db.Column(db.Integer)
-    triples = db.Column(db.Integer)
-    hr = db.Column(db.Integer)
-    rbi = db.Column(db.Integer)
-    so = db.Column(db.Integer)
-    era = db.Column(db.String)
-    slg = db.Column(db.String)
-    wins = db.Column(db.Integer)
-    losses = db.Column(db.Integer)
-    pct = db.Column(db.String)
-    rs = db.Column(db.Integer)
-    ra = db.Column(db.Integer)
-
-    def __init__(self, time, season, teamid, name, stars, stadium, pl, pl_position,
-                 trained, games, ab, hits, avg, singles, doubles, triples,
-                 hr, rbi, so, era, slg, wins, losses, pct, rs, ra):
-        self.time = time
-        self.season = season
-        self.teamid = teamid
-        self.name = name
-        self.stars = stars
-        self.stadium = stadium
-        self.pl = pl
-        self.pl_position = pl_position
-        self.trained = trained
-        self.games = games
-        self.ab = ab
-        self.hits = hits
-        self.avg = avg
-        self.singles = singles
-        self.doubles = doubles
-        self.triples = triples
-        self.hr = hr
-        self.rbi = rbi
-        self.so = so
-        self.era = era
-        self.slg = slg
-        self.wins = wins
-        self.losses = losses
-        self.pct = pct
-        self.rs = rs
-        self.ra = ra
-
-class Player(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    season = db.Column(db.Integer)
-    teamid = db.Column(db.Integer)
-    playerid = db.Column(db.Integer)
-    name = db.Column(db.String)
-    position = db.Column(db.String)
-    games = db.Column(db.Integer)
-    ab = db.Column(db.Integer)
-    hits = db.Column(db.Integer)
-    avg = db.Column(db.String)
-    singles = db.Column(db.Integer)
-    doubles = db.Column(db.Integer)
-    triples = db.Column(db.Integer)
-    hr = db.Column(db.Integer)
-    rbi = db.Column(db.Integer)
-    so = db.Column(db.Integer)
-    era = db.Column(db.String)
-    slg = db.Column(db.String)
-
-    def __init__(self, season, teamid, playerid, name, position, games, ab, hits,
-                 avg, singles, doubles, triples, hr, rbi, so, era, slg):
-        self.season = season
-        self.teamid = teamid
-        self.playerid = playerid
-        self.name = name
-        self.position = position
-        self.games = games
-        self.ab = ab
-        self.hits = hits
-        self.avg = avg
-        self.singles = singles
-        self.doubles = doubles
-        self.triples = triples
-        self.hr = hr
-        self.rbi = rbi
-        self.so = so
-        self.era = era
-        self.slg = slg
-
-class Pitcher(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    season = db.Column(db.Integer)
-    teamid = db.Column(db.Integer)
-    playerid = db.Column(db.Integer)
-    name = db.Column(db.String)
-    games = db.Column(db.Integer)
-    so = db.Column(db.Integer)
-    era = db.Column(db.String)
-
-    def __init__(self, season, teamid, playerid, name, games, so, era):
-        self.season = season
-        self.teamid = teamid
-        self.playerid = playerid
-        self.name = name
-        self.games = games
-        self.so = so
-        self.era = era
-
+with app.app_context():
+    db.init_app(app)
 
 @app.route('/')
 def index():
@@ -143,7 +28,7 @@ def stats(tid):
     seasons = Team.query.filter_by(teamid=tid).order_by(Team.season.desc()).all()
     team = Team.query.filter_by(teamid=tid, season=current_season).first()
 
-    if team and (team.time - datetime.now()).total_seconds() > -600:
+    if team and (team.time - datetime.now()).total_seconds() > -3600:
         players = Player.query.filter_by(teamid=tid, season=current_season).all()
         pitchers = Pitcher.query.filter_by(teamid=tid, season=current_season).all()
         updated = team.time.strftime('Updated on %B %d at %I:%M %p')
@@ -247,16 +132,16 @@ def seasonstats(tid, season):
     if season != 'all' and int(current_season) == int(season):
 
         return redirect('/stats/{0}'.format(tid))
-    
+
     else:
         if season == "all":
-            team = Team.query.filter_by(teamid=tid).order_by(Team.season.desc()).all()
+            team = Team.query.filter_by(teamid=tid).order_by(Team.season.asc()).all()
             players = Player.query.filter_by(teamid=tid).all()
             pitchers = Pitcher.query.filter_by(teamid=tid).all()
             stats = Team.query.filter_by(teamid=tid).order_by(Team.season.desc()).first()
 
             return render_template('allstats.html', team=team, stats=stats, players=players, pitchers=pitchers)
-        
+
         else:
             team = Team.query.filter_by(teamid=tid).order_by(Team.season.desc()).all()
             players = Player.query.filter_by(teamid=tid, season=season).all()
@@ -267,6 +152,17 @@ def seasonstats(tid, season):
                     stats = entry
 
             return render_template('seasonstats.html', team=team, stats=stats, players=players, pitchers=pitchers)
+
+@app.route('/stats/<tid>/career/<playerid>')
+def careerstats(tid, playerid):
+    current_season = sbteamdata.get_current_season()
+    team = Team.query.filter_by(teamid=tid, season=current_season).first()
+    teamavg = Team.query.filter_by(teamid=tid).order_by(Team.season.asc()).all()
+    players = Player.query.filter_by(teamid=tid, season=current_season).all()
+    seasons = Player.query.filter_by(teamid=tid, playerid=playerid).all()
+    total = getcareerstats.get_career_stats(tid, playerid)
+
+    return render_template('careerstats.html', total=total, seasons=seasons, team=team, playerid=playerid, players=players, teamavg=teamavg)
 
 @app.route('/leaders')
 def leaders():
